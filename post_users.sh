0046b4cbc4ab67bb91a4efbdb93ea02a6292ae36
Brittany Mazza <bmazza555@gmail.com>
2014-06-15 12:39:06 -0700

    add script to post users to server

---
+++ post_users.sh
@@ -0,0 +1,3 @@
+curl -H "Content-Type: application/json" -X POST -d '{"username":"Brittany", "password":"friends"}' http://localhost:8080/users
+curl -H "Content-Type: application/json" -X POST -d '{"username":"Philip", "password":"fri"}' http://localhost:8080/users
+curl -H "Content-Type: application/json" -X POST -d '{"username":"Entry 3!", "password":"fri"}' http://localhost:8080/users
